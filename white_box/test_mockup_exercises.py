fa0cb2281aa0f24b37b25a2aeb31877844004812
contrererick-ids <is722185@iteso.mx>
2025-10-02 18:55:38 -0600

    test: Add unit test for Exercise
    Added TestReadDataFromFile in test_mockup_exercises.py to verify
    the behavior of the read_data_from_file function on successful read

--- white_box/test_mockup_exercises.py
+++ white_box/test_mockup_exercises.py
@@ -6,7 +6,7 @@ Mock up testing examples.
 import unittest
 from unittest.mock import patch
 
-from white_box.mockup_exercises import fetch_data_from_api
+from white_box.mockup_exercises import fetch_data_from_api, read_data_from_file
 
 
 class TestDataFetcher(unittest.TestCase):
@@ -60,3 +60,29 @@ class TestDataFetcher(unittest.TestCase):
 #
 #         # Verify data is what we expect
 #         mock_print.assert_called_once_with("Hello, World!")
+
+
+class TestReadDataFromFile(unittest.TestCase):
+    """
+    Read data from file unittest class.
+    """
+
+    @patch("builtins.open", create=True)
+    def test_read_data_from_file_success(self, mock_open):
+        """
+        Success case.
+        """
+        # Set up the mock file object
+        mock_file = mock_open.return_value.__enter__.return_value
+
+        # Define the content to be returned when the file is read
+        mock_file.read.return_value = "Sample data from file."
+
+        # Call the function under test
+        result = read_data_from_file("sample.txt")
+
+        # Assert that the function returns the expected result
+        self.assertEqual(result, "Sample data from file.")
+
+        # Assert that open was called with the correct filename and encoding
+        mock_open.assert_called_once_with("sample.txt", encoding="utf-8")
