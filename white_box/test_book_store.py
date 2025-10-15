8bcf82e0527b60b0603f4e7617c4e0d7fe2892ff
contrererick-ids <is722185@iteso.mx>
2025-10-15 11:06:27 -0600

    test: Add unit tests for BookStore
    Added test cases to verify behavior of functions on the BookStore
    exercise class

--- white_box/test_book_store.py
+++ white_box/test_book_store.py
@@ -57,3 +57,119 @@ class TestBookStore(unittest.TestCase):
         """
         book_store = BookStore()
         self.assertEqual(book_store.books, [])
+
+    @patch("builtins.print")
+    def test_add_book(self, mock_print):
+        """
+        Test the add_book function to append a book to the BookStore array.
+        """
+
+        title = "1984"
+        author = "George Orwell"
+        price = 3.5
+        quantity = 2
+        book = Book(title, author, price, quantity)
+        book_store = BookStore()
+        book_store.add_book(book)
+        mock_print.assert_any_call(f"Book '{book.title}' added to the store.")
+
+    @patch("builtins.print")
+    def test_display_books_in_book_store_no_empty_array(self, mock_print):
+        """
+        Test the display_books function to print all the books in the array
+        when it's not empty.
+        """
+
+        title = "1984"
+        author = "George Orwell"
+        price = 3.5
+        quantity = 2
+        book = Book(title, author, price, quantity)
+        book_store = BookStore()
+        book_store.add_book(book)
+        book_store.display_books()
+        mock_print.assert_has_calls(
+            [
+                unittest.mock.call(f"Title: {book.title}"),
+                unittest.mock.call(f"Author: {book.author}"),
+                unittest.mock.call(f"Price: ${book.price}"),
+                unittest.mock.call(f"Quantity: {book.quantity}"),
+            ]
+        )
+
+    @patch("builtins.print")
+    def test_display_books_in_book_store_with_empty_array(self, mock_print):
+        """
+        Test the display_books function to print all the books in the array
+        when it's empty.
+        """
+
+        book_store = BookStore()
+        book_store.display_books()
+        mock_print.assert_any_call("No books in the store.")
+
+    @patch("builtins.print")
+    def test_search_book_with_empty_array(self, mock_print):
+        """
+        Test the search_book function with an empty array.
+        """
+
+        title = "1984"
+        book_store = BookStore()
+        book_store.search_book(title)
+        mock_print.assert_any_call(f"No book found with title '{title}'.")
+
+    @patch("builtins.print")
+    def test_search_book_with_no_empty_array_and_title_exists(self, mock_print):
+        """
+        Test the search_book function with no empty array and the title exists.
+        """
+
+        title = "1984"
+        author = "George Orwell"
+        price = 3.5
+        quantity = 2
+        book = Book(title, author, price, quantity)
+        book_store = BookStore()
+        book_store.add_book(book)
+        book_store.search_book(title)
+        found_count = len(
+            [book for book in book_store.books if book.title.lower() == title.lower()]
+        )
+        mock_print.assert_has_calls(
+            [
+                unittest.mock.call(
+                    f"Found {found_count} book(s) with title '{title}':"
+                ),
+                unittest.mock.call(f"Title: {book.title}"),
+                unittest.mock.call(f"Author: {book.author}"),
+                unittest.mock.call(f"Price: ${book.price}"),
+                unittest.mock.call(f"Quantity: {book.quantity}"),
+            ]
+        )
+
+    @patch("builtins.print")
+    def test_search_book_with_no_empty_array_and_title_does_not_exists(
+        self, mock_print
+    ):
+        """
+        Test the search_book function with no empty array and the title does not exists.
+        """
+
+        title = "1984"
+        author = "George Orwell"
+        price = 3.5
+        quantity = 2
+        book = Book(title, author, price, quantity)
+        book_store = BookStore()
+        book_store.add_book(book)
+        title_to_search = "Farenheit 451"
+        book_store.search_book(title_to_search)
+        found_count = len(
+            [
+                book
+                for book in book_store.books
+                if book.title.lower() == title_to_search.lower()
+            ]
+        )
+        mock_print.assert_any_call(f"No book found with title '{title_to_search}'.")
