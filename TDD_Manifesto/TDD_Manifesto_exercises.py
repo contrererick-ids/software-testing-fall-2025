fd8993bffd477289e4e15b5eae3ec39e60a9e686
contrererick-ids <is722185@iteso.mx>
2025-10-20 12:51:46 -0600

    chore: Add TDD Manifesfo Folder
    Added a new TDD_Manifesto folder with two files for working on exercises
    and tests of the TDD Manifesto website.

---
+++ TDD_Manifesto/TDD_Manifesto_exercises.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+TDD Manifesto exercises
+"""
+
+def fizz_buzz(num):
+    return str(num)
---
+++ TDD_Manifesto/test_TDD_Manifesto_exercises.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+"""
+TDD Manifesto unit testing examples
+"""
+
+import unittest
+from TDD_Manifesto.TDD_Manifesto_exercises import(
+    fizz_buzz,
+)
+
+
+class TestFizzBuzz(unittest.TestCase):
+    """
+    FizzBuzz unittest class.
+    """
+
+    def test_GivenANumberAsInput_whenFizzBuzzFunctionGetsTheInput_thenTheNumberIsPrintAsSring(self):
+        
+        number_as_input = 0
+
+        test_string_output = fizz_buzz(number_as_input)
+
+        self.assertEqual(test_string_output,"0")
+
+    def test_GivenACharacterAsInput_whenFizzBuzzFunctionGetsTheInput_thenTheFunctionFails(self):
+
+        character_as_input = "a"
+
+        self.assertRaises(ValueError, fizz_buzz, character_as_input)
+
+    def test_GivenAMultipleOfThreeAsInput_whenFizzBuzzFunctionGetsTheInput_thenTheFunctionPrintsFizz(self):
+
+        number_as_input = 0
+
+        test_string_output = fizz_buzz(number_as_input)        
+
+        self.assertEqual(test_string_output, "Fizz")
+
+    def test_GivenAMultipleOfFiveAsInput_whenFizzBuzzFunctionGetsTheInput_thenTheFunctionPrintsBuzz(self):
+
+        number_as_input = 5
+
+        test_string_output = fizz_buzz(number_as_input)        
+
+        self.assertEqual(test_string_output, "Buzz")
+
+    def test_GivenAMultipleOfThreeAndFiveAsInput_whenFizzBuzzFunctionGetsTheInput_thenTheFunctionPrintsFizzBuzz(self):
+
+        number_as_input = 15
+
+        test_string_output = fizz_buzz(number_as_input)        
+
+        self.assertEqual(test_string_output, "FizzBuzz")
